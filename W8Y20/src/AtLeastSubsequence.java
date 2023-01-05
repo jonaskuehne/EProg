b67d4e7387a84506aee2b01cb127fda61da734f5
jonaskuehne <jonaskuehne2001@gmail.com>
2023-01-05 09:52:59 +0100

    w8 erledigt.

--- W8Y20/src/AtLeastSubsequence.java
+++ W8Y20/src/AtLeastSubsequence.java
@@ -1,13 +1,41 @@
-
+import java.util.ArrayList;
 
 public class AtLeastSubsequence {
 
 	public static boolean subsequence(String s, String t, int n) {
-        return n <= f(s, t);
+		
+		ArrayList<String> combs = new ArrayList<>();
+		combs.add("");
+		
+		getCombs(combs, s, t);
+		
+		int count = 0;
+		
+		for (String comb : combs) {
+			if (comb.equals(t)) {
+				++count;
+			}
+		}
+		
+        return n <= count;
     }
-
-    static int f(String s, String t) {
-    	return t.isEmpty() ? 1 : s.isEmpty() ? 0 : f(s.substring(1), t) + (s.charAt(0) == t.charAt(0) ? f(s.substring(1), t.substring(1)) : 0);
-    }
+	
+	// faster than you would think my friend
+	public static void getCombs(ArrayList<String> store, String s, String t) {
+		if (s.length() == 0) {
+			return;
+		}
+		
+		int l = store.size();
+		
+		for (int i = 0; i < l; ++i) {
+			// here's where the magic happens
+			if (t.contains(s.charAt(0)+ "") && store.get(i).length() < t.length()) {
+				store.add(store.get(i) + s.charAt(0));
+			}
+		}
+		
+		getCombs(store, s.substring(1), t);
+	}
 }
 
