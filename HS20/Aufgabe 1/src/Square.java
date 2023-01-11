b31531daed44975a87ae2835f9a82d8b7aae56e1
jonaskuehne <jonaskuehne2001@gmail.com>
2023-01-11 10:20:47 +0100

    init

---
+++ HS20/Aufgabe 1/src/Square.java
@@ -0,0 +1,21 @@
+public class Square {
+	
+    public static void main(String[] args) {
+    	int[][] t = { 
+    			{0, 3, 5, 7, 2}, 
+    			{2, 7, 4, 3, 0}, 
+    			{4, 4, 6, 1, 4}, 
+    			{7, 7, 4, 3, 3}, 
+    			{0, 3, 5, 7, 2} 
+    		};
+
+    	System.out.println(checkProperty(t));
+    }
+
+    static boolean checkProperty(int[][] x) {
+    	//TODO
+    	return false;
+    }
+
+}
+    
