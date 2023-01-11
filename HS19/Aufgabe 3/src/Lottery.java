698ba3cae05306b0afe1dacb8b84d0c09742382c
jonaskuehne <jonaskuehne2001@gmail.com>
2023-01-11 16:19:58 +0100

    Lorrety erledigt

--- HS19/Aufgabe 3/src/Lottery.java
+++ HS19/Aufgabe 3/src/Lottery.java
@@ -1,27 +1,84 @@
 import java.util.*;
 
 public class Lottery {
+	int ticketCount;
+	int maxNumber;
+	Set<Integer> winningNums;
+	boolean drawn;
+	
+	Set<Ticket> tickets;
 
 	public Lottery(int maxNumber) {
-		// TODO
+		this.maxNumber = maxNumber;
+		this.ticketCount = 1;
+		tickets = new HashSet<>();
 	}
 
 	public Ticket buyTicket(int[] numbers) {
-		// TODO
-		return null;
+		
+		if (drawn) {
+			throw new IllegalStateException();
+		}
+		Ticket ticket = new Ticket(numbers, ticketCount);
+		
+		Arrays.sort(numbers);
+		
+		if (numbers[0] < 1 || numbers[numbers.length - 1] > maxNumber || ticket.getNumbers().size() != 6) {
+			throw new IllegalArgumentException();
+		}
+		
+		tickets.add(ticket);
+		
+		++ticketCount;
+		return ticket;
 	}
 
 	public int soldTickets() {
-		// TODO
-		return 0;
+		return ticketCount - 1;
+	}
+	
+	public static void main(String[] args) {
+		Lottery lot = new Lottery(10);
+		
+		Ticket t1 = lot.buyTicket(new int[] {1, 2, 3, 4, 5, 6});
+		
+		System.out.println(t1.getNumbers());
+		
+		lot.draw();
+		
+		System.out.println(t1.getCorrectNumbers());
+		
 	}
 
 	public void draw() {
-		// TODO
+		
+		if (drawn) {
+			throw new IllegalStateException();
+		}
+		
+		drawn = true;
+		winningNums = new HashSet<>();
+		
+		Random rand = new Random();
+
+		while(winningNums.size() < 6) {
+			int num = rand.nextInt(maxNumber);
+			winningNums.add(num + 1);
+		}
+		
+		for (Ticket t : tickets) {
+			t.drawn = true;
+			t.correctNums = winningNums;
+		}
+		
 	}
 
 	public Set<Integer> getWinning() {
-		// TODO
-		return null;
+		
+		if (!drawn) {
+			throw new IllegalStateException();
+		}
+		
+		return winningNums;
 	}
 }
--- HS19/Aufgabe 3/src/Ticket.java
+++ HS19/Aufgabe 3/src/Ticket.java
@@ -1,24 +1,68 @@
 import java.util.*;
 
 public class Ticket {
-
+	
+	int code;
+	boolean drawn;
+	Set<Integer> nums;
+	Set<Integer> correctNums;
+	
+	// ctor
+	public Ticket(int[] numbers, int code) {
+		drawn = false;
+		this.code = code;
+		nums = new HashSet<>();
+		
+		for (int i : numbers) {
+			nums.add(i);
+		}
+		
+	}
+	
 	public int getNumber() {
-		// TODO
-		return 0;
+		return code;
 	}
 
 	public Set<Integer> getNumbers() {
-		// TODO
-		return null;
+		return nums;
 	}
 
 	public Set<Integer> getCorrectNumbers() {
-		// TODO
-		return null;
+		
+		if (!drawn) {
+			throw new IllegalStateException();
+		}
+		
+		// maybe more cash w/ the other version who knows
+		Set<Integer> onlyNums = new HashSet<>();
+		
+		for (int i : correctNums) {
+			if (nums.contains(i)) {
+				onlyNums.add(i);
+			}
+		}
+		
+		return onlyNums;
 	}
 
 	public int getPrize() {
-		// TODO
-		return 0;
+		
+		if (!drawn) {
+			throw new IllegalStateException();
+		}
+		
+		int numSame = 0;
+		
+		for (int i : nums) {
+			if (correctNums.contains(i)) {
+				++numSame;
+			}
+		}
+		
+		if (numSame == 0) {
+			return 0;
+		}
+		
+		return (int) (Math.pow(20, numSame - 1) * 5);
 	}
 }
