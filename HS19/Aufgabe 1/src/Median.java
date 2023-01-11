21ea01720a1a69a30d72acc7c26a947b9f32e5c8
jonaskuehne <jonaskuehne2001@gmail.com>
2023-01-11 14:49:43 +0100

    median erledigt

--- HS19/Aufgabe 1/src/Median.java
+++ HS19/Aufgabe 1/src/Median.java
@@ -2,7 +2,28 @@ import java.util.*;
 
 public class Median {
 	public static double median(Scanner scanner) {
-		// TODO
-		return 0;
+		
+		List<Integer> list = new ArrayList<>();
+		
+		while (scanner.hasNextInt()) {
+			list.add(scanner.nextInt());
+		}
+		
+		list.sort(new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				return o1.compareTo(o2);
+			}
+			
+		});
+		
+		
+		if (list.size() % 2 == 1) {
+			return list.get(list.size() / 2);
+		} else {
+			return (double)(list.get(list.size() / 2) + list.get(list.size() / 2 - 1)) / 2;
+		}
+		
 	}
 }
--- HS19/Aufgabe 1/test/MedianTest.java
+++ HS19/Aufgabe 1/test/MedianTest.java
@@ -12,4 +12,25 @@ public class MedianTest {
 		double result = Median.median(input);
 		assertEquals(3, result, 0.0001);
 	}
+	
+	public void testMedian1() {
+		Scanner input = new Scanner("1 5 4 3 0 4");
+		double result = Median.median(input);
+		assertEquals(3.5, result, 0.0001);
+	}
+	
+	public void testMedian2() {
+		Scanner input = new Scanner("1");
+		double result = Median.median(input);
+		assertEquals(1, result, 0.0001);
+	}
+	
+	public void testMedian3() {
+		Scanner input = new Scanner("1 2");
+		double result = Median.median(input);
+		assertEquals(1.5, result, 0.0001);
+	}
+	
+	
+	
 }
