b8222f6c0bff2f3d5ff2d87b707d7b14accf4a9e
jonaskuehne <jonaskuehne2001@gmail.com>
2023-01-16 15:14:18 +0100

    Grid erledigt

--- HS20/Aufgabe 2/src/Grid.java
+++ HS20/Aufgabe 2/src/Grid.java
@@ -16,8 +16,71 @@ public class Grid {
 		System.out.println("Executed main for Grid");
 	}
 	
+	// I think we can assume that our grid before the function call is valid
 	public static void adjustGrid(Node origin, int A, int B) {
-		//TODO
+		// get existing grid
+		Node[][] g = new Node[A][B];
+		
+		// get grid
+		g[0][0] = origin;
+		
+		// fill first row (or what we have)
+		int y = 1;
+		while (y < B && g[0][y - 1].getRight() != null) {
+			g[0][y] = g[0][y - 1].getRight();
+			++y;
+		}
+		
+		// fill whole array (or what we have)
+		int x = 1;
+		while (x < A && g[x - 1][0].getBottom() != null) {
+			for (int i = 0; i < y; ++i) {
+				g[x][i] = g[x - 1][i].getBottom();
+			}
+			++x;
+		}
+		
+		// not done with columns
+		for (;y < B; ++y) {
+			for (int i = 0; i < x; ++i) {
+				// new object
+				g[i][y] = new Node(null, null);
+				// link from right
+				g[i][y - 1].setRight(g[i][y]);
+				// link from above if necessary
+				if (i > 0) {
+					g[i - 1][y].setBottom(g[i][y]);
+				}
+				
+			}
+		}
+		
+		// not done with rows
+		for (;x < A; ++x) {
+			// can assume that all columns are filled
+			for (int i = 0; i < B; ++i) {
+				// new node
+				g[x][i] = new Node(null, null);
+				// link from above
+				g[x - 1][i].setBottom(g[x][i]);
+				// link from right if necessary
+				if (i > 0) {
+					g[x][i - 1].setRight(g[x][i]);
+				}
+			}
+		}
+		
+		// cut edges
+		for (int i = 0; i < A; ++i) {
+			// nodes on right to null
+			g[i][B - 1].setRight(null);
+		}
+		
+		for (int i = 0; i < B; ++i) {
+			// nodes on bottom to null
+			g[A - 1][i].setBottom(null);
+		}
+
 	}
 
 }
